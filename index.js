a397281f59ec0da0621f4eeaa268cd09dd14e6ac
Xocash695 <akash.kallumkal@gmail.com>
2022-07-12 11:39:13 -0400

    added compression

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const compression = require('compression')
+
+app.use(compression())
 
 app.use(express.static(path.join(__dirname, 'build')));
 
